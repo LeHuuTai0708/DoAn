2fb0d7ad3cee5dbda1328dbed95e59d2819aab48
LeHuuTai7898 <tailh07081998@gmail.com>
2020-05-08 08:42:33 +0700

    Fist Commit

---
+++ TNTRecruitment/src/pages/recruitment/recruitment.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the RecruitmentPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-recruitment',
+  templateUrl: 'recruitment.html',
+})
+export class RecruitmentPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RecruitmentPage');
+  }
+
+}
