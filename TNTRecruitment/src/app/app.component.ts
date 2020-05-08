6358618e1c25f636d41e404329da0ce66761bf97
Death <nguyennhac3006@gmail.com>
2020-05-08 11:27:19 +0700

    FirstScreen

--- TNTRecruitment/src/app/app.component.ts
+++ TNTRecruitment/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { FirstPage } from '../pages/first/first';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage:any = FirstPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
