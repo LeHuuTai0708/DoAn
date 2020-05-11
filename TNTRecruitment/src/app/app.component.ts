4799d00c84a5270338d7f57635580bba2572d94b
LeHuuTai7898 <tailh07081998@gmail.com>
2020-05-11 09:06:27 +0700

    save ID user when login

--- TNTRecruitment/src/app/app.component.ts
+++ TNTRecruitment/src/app/app.component.ts
@@ -3,21 +3,31 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Storage } from '@ionic/storage';
 
 import { HomePage } from '../pages/home/home';
 import { FirstPage } from '../pages/first/first';
+import { LoginPage } from '../pages/login/login';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = FirstPage;
+  rootPage: any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public storage: Storage) {
+    console.log(this.storage.get("AccountID"));
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      this.storage.get("AccountID").then((data) => {
+        if (data) {
+          this.rootPage = TabsPage;
+        } else {
+          this.rootPage = FirstPage;
+        }
+      });
     });
   }
 }
