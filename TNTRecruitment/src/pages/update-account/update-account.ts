a8bd405b67a360b48594fe6e136402e74d2f3af6
LeHuuTai7898 <tailh07081998@gmail.com>
2020-05-18 16:25:48 +0700

    fix UI

--- TNTRecruitment/src/pages/update-account/update-account.ts
+++ TNTRecruitment/src/pages/update-account/update-account.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, App } from 'ionic-angular';
+import { RestProvider } from '../../providers/rest/rest';
+import { Student, Company, UsersAccount } from '../../Models/Users';
+import { Storage } from '@ionic/storage';
 
 /**
  * Generated class for the UpdateAccountPage page.
@@ -14,12 +17,42 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'update-account.html',
 })
 export class UpdateAccountPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public settingUserInfo: boolean;
+  user: UsersAccount;
+  student: Student;
+  company: Company;
+  id: String;
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public storage: Storage, private app: App, public restProvider: RestProvider) {
+    this.storage.get("AccountID").then((data) => {
+      this.id = data;
+      this.restProvider.getUserInfomation(this.id).then((data) => {
+        this.user = JSON.parse(JSON.stringify(data));
+        this.SettingInfo().then((data) => {
+          this.settingUserInfo = data;
+        });
+      });
+    });
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UpdateAccountPage');
+  SettingInfo(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      if (this.user != null) {
+        switch (this.user.type) {
+          case "student":
+            this.restProvider.getStudentInfomation(this.id).then((data) => {
+              this.student = JSON.parse(JSON.stringify(data));
+              resolve(true);
+            });
+            break;
+          case "company":
+            this.restProvider.getCompanyInfomation(this.id).then((data) => {
+              this.company = JSON.parse(JSON.stringify(data));
+              resolve(false);
+              console.log(this.company);
+            });
+            break;
+        }
+      }
+    });
   }
-
 }
