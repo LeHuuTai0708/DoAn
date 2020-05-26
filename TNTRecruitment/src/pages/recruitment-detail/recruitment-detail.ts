95f54c244032acbfacf5a3f6641e3485f0f33e83
LeHuuTai7898 <tailh07081998@gmail.com>
2020-05-26 13:31:29 +0700

    fix something

--- TNTRecruitment/src/pages/recruitment-detail/recruitment-detail.ts
+++ TNTRecruitment/src/pages/recruitment-detail/recruitment-detail.ts
@@ -21,20 +21,25 @@ import { ListApplicationPage } from '../list-application/list-application';
 export class RecruitmentDetailPage {
   tabBarElement: any;
   id: string;
+  idRec: string;
   user: UsersAccount;
   check: boolean;
   recruitment: Recruitment;
   userList: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public storage: Storage, private app: App, public restProvider: RestProvider) {
-    this.id = this.navParams.get("id");
-    this.restProvider.getRecruitmentInfomation(this.id).then((data) => {
-      this.recruitment = JSON.parse(JSON.stringify(data));
-      console.log(this.recruitment);
-      this.restProvider.getUserInfomation(this.recruitment.companyid).then((data) => {
-        this.user = JSON.parse(JSON.stringify(data));
-        this.SettingRecruitment().then((data) => {
-          this.check = data;
+    this.idRec = this.navParams.get("id");
+    this.storage.get("AccountID").then((data) => {
+      this.id = data;
+      this.restProvider.getRecruitmentInfomation(this.idRec).then((data) => {
+        this.recruitment = JSON.parse(JSON.stringify(data));
+        this.restProvider.getUserInfomation(this.id).then((data) => {
+          this.user = JSON.parse(JSON.stringify(data));
+          console.log(this.user.type);
+          this.SettingRecruitment().then((data) => {
+            this.check = data;
+            console.log(this.check);
+          });
         });
       });
     });
@@ -42,9 +47,10 @@ export class RecruitmentDetailPage {
   SettingRecruitment(): Promise<boolean> {
     return new Promise((resolve, reject) => {
       if (this.user.type == "student") {
-        resolve(true);
-      } else
         resolve(false);
+      } else if (this.user.type == "company") {
+        resolve(true);
+      }
     });
   }
   takeMeBack() {
